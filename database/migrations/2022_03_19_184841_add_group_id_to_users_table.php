dd0a76aa0b71c4004e8df25a5975e0a3f6e2d6ea
uncleben006 <uncleben006@gmail.com>
2022-04-02 12:09:58 +0800

    Update group id migration

--- database/migrations/2022_03_19_184841_add_group_id_to_users_table.php
+++ database/migrations/2022_03_19_184841_add_group_id_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('group_id');
+            $table->integer('group_id')->nullable();
         });
     }
 
