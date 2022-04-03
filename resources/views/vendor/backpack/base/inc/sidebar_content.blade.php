8549ff1cea401809445f77d5fa6383419ed1262e
uncleben006 <uncleben006@gmail.com>
2022-04-04 02:43:23 +0800

    Add permission restrictions to the sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,11 +1,15 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
-<!-- Node servers -->
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('node') }}'><i class='nav-icon la la-server'></i>節點管理</a></li>
+@if(backpack_user()->can('nodes'))
+    <!-- Node servers -->
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('node') }}'><i class='nav-icon la la-server'></i>節點管理</a></li>
+@endif
 
-<!-- Groups -->
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('group') }}'><i class='nav-icon la la-object-group'></i>群組管理</a></li>
+@if(backpack_user()->can('groups'))
+    <!-- Groups -->
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('group') }}'><i class='nav-icon la la-object-group'></i>群組管理</a></li>
+@endif
 
 <!-- Tags -->
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon la la-tags'></i>標籤管理</a></li>
